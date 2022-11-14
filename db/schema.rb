5361f467e04ce1e76a4fb05cd3c3cec2bb8f8bf0
Benjamin Croad <benjamincroadapple@gmail.com>
2022-11-14 13:55:33 +0000

    Added functioning (but not particularly styled...) integration of stripe checkout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_142806) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_12_140258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_11_142806) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "checkouts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
