58266d8dd955300ad2fced3a20088f37cb6906e6
Benjamin Croad <benjamincroadapple@gmail.com>
2022-11-15 17:30:08 +0000

    Added stimulus controller and action to cart

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import CartController from "./cart_controller"
+application.register("cart", CartController)
